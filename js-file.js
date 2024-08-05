d18e2e512833e5a114641d20922ad43db2729d23
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-05 14:35:48 -0400

    Shopping list

--- js-file.js
+++ js-file.js
@@ -1,8 +1,10 @@
 const para = document.createElement("p");
+const bod = document.getElementById('bod')
 para.textContent="Hey I'm Red!";
-
+bod.appendChild(para)
 const header = document.createElement("h3");
 header.textContent="I'm a blue h3!";
+bod.appendChild(header)
 const div = document.createElement("div");
 div.style.border = "thick solid black";
 div.style.backgroundColor = "pink";
@@ -11,5 +13,6 @@ div.appendChild(header1);
 header1.textContent= "I'm in a div";
 const divp = document.createElement("p");
 div.appendChild(divp);
+bod.appendChild(div)
 divp.textContent= "ME TOO!";
 console.log("working?")
